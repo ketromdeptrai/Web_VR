b695cea5a44a9d0065095c922b9b1e8e5fcf73ba
Phu NM <ketromdeptrai@gmail.com>
2017-09-26 22:55:05 +0700

    modify sth

--- StartSocketIOServer.sh
+++ StartSocketIOServer.sh
@@ -7,6 +7,6 @@ ReapeatTime=$2;
 while ((ReapeatTime--)); do
 	((index++))
 	OutputName=$VideoName'_'$index'.txt'
-	node ./server/socket_server.js | tee $OutputName
+	node ./server/socket_server.js | tee ./head_tracking_log/$OutputName
 done
 $SHELL
